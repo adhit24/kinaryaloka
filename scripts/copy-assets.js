9dc4dc6cad09b340d3b64dc30d9be934ad7bba45
Adhitya Nugraha <adhit24@gmail.com>
2026-05-26 16:36:50 +0700

    fix: use ESM import in copy-assets.js to fix require is not defined error

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import path from 'path'
 
 function copyDir(src, dest) {
   if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true })
